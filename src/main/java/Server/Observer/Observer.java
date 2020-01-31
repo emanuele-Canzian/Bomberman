f20039e973fa91d279c44ea716d97f828298e0b9
emanuele <emanuele.canzian@csbe.ch>
2020-01-31 21:08:10 +0100

    final Commit

---
+++ src/main/java/Server/Observer/Observer.java
@@ -0,0 +1,18 @@
+package Server.Observer;
+
+public class Observer{
+
+    private int clientId;
+
+    public void setClientId(int clientId){
+        this.clientId = clientId;
+    }
+
+    public int getClientId(){
+        return clientId;
+    }
+
+    public void update(String message) {
+        System.out.println("Hallo Client " + getClientId() + " " + message);
+    }
+}
---
+++ src/main/java/Server/Observer/Subject.java
@@ -0,0 +1,13 @@
+package Server.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+
+    int clientId = 1;
+
+    private List<Observer> observers = new ArrayList<Observer>();
+
+
+
